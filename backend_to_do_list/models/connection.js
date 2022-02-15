d3281ec368a34de9b732018288aee3864e62d356
Rafael Victor <rafavictorgs@gmail.com>
2022-02-15 17:15:54 -0300

    Build: Installing dependencies for start application

---
+++ backend_to_do_list/models/connection.js
@@ -0,0 +1,16 @@
+const mongodb = require('mongodb').MongoClient;
+
+require('dotenv').config();
+
+const MONGO_DB_URL = procces.env.MONGO_DB_URL || 'mongodb://localhost:27017';
+const DB_NAME = 'toDoList';
+
+module.exports = () => mongodb.connect(MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((connection) => connection.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
