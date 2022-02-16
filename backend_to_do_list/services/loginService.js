9cbcdc8406ded50c7327819c4fb01b6e32114854
Rafael Victor <rafavictorgs@gmail.com>
2022-02-16 19:43:52 -0300

    feat: adding validations for login route

--- backend_to_do_list/services/loginService.js
+++ backend_to_do_list/services/loginService.js
@@ -1,7 +1,16 @@
 const models = require('../models/loginModel');
+const { notFound } = require('../utils/statusCode');
+const validations = require('./validations/loginValidations');
+const errorHandler = require('../middlewares/errorHandler');
 
 const loginUser = async (body) => {
+  const { email, password } = body;
+
+  validations.loginValidation(email, password);
+
   const response = await models.loginUser(body);
+
+  if (!response) throw errorHandler(notFound, { message: 'Usuário não encontrado!' });
 
   return response;
 };
